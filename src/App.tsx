54575fcdc6152a3d1ab73a545168b421cabf47ff
Egor Anakhin <eanahin8v@mail.ru>
2025-02-20 21:17:59 +0300

    feat: text loading status animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-function App() {
+import { TextLoadingStatus } from "./animations/TextLoadingStatus.tsx";
 
+function App() {
   return (
-    <div className="text-xl mt-10">
-      123123123
+    <div className="text-xl mt-10 flex w-[100vw] h-[100vh] bg-gray-900 justify-center items-center">
+      <TextLoadingStatus
+        textColor={"#fff000"}
+        gradientColor={"#ff0000"}
+        textAnimationDuration={1.6}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/animations/TextLoadingStatus.tsx
@@ -0,0 +1,84 @@
+import { FC, HTMLProps, useEffect, useState } from "react";
+import { TextLoop } from "../components/ui/text-loop.tsx";
+import { TextShimmerWave } from "../components/ui/text-shimmer-wave.tsx";
+
+interface LoadingStatusProps extends HTMLProps<HTMLParagraphElement> {
+  textColor?: string;
+  gradientColor?: string;
+  className?: string;
+  loopDuration?: number;
+  textAnimationDuration?: number;
+  textAnimationRepeat?: number;
+  texts?: string[];
+  spread?: number;
+  interval?: number;
+  transitionDuration?: number;
+  fontSize?: string;
+  fontWeight?: string;
+  textAlign?: "left" | "center" | "right";
+  lineHeight?: string;
+  letterSpacing?: string;
+  containerClassName?: string;
+}
+
+export const TextLoadingStatus: FC<LoadingStatusProps> = ({
+  textColor = "#00ffff",
+  gradientColor = "#f1f1f1",
+  loopDuration = 2,
+  className = "",
+  textAnimationRepeat = 0,
+  texts = [
+    "Сканируем ваш профиль",
+    "Проверяем ваши настройки",
+    "Смотрим прогноз погоды",
+    "Ищем лучший вариант решения",
+  ],
+  textAnimationDuration = 1,
+  spread = 2,
+  interval = loopDuration,
+  transitionDuration = 0.2,
+  containerClassName = "",
+  ...rest
+}) => {
+  const [initialTexts, setInitialTexts] = useState<string[]>(["", ...texts]);
+  const [isFirstRender, setIsFirstRender] = useState(true);
+
+  useEffect(() => {
+    if (isFirstRender) {
+      setIsFirstRender(false);
+      setInitialTexts(texts);
+    }
+  }, [isFirstRender, texts]);
+
+  const gradientColorStyle: string =
+    " [--base-gradient-color:" + gradientColor + "] ";
+  const textColorStyle: string = " [--base-color:" + textColor + "] ";
+
+  return (
+    <div className={containerClassName}>
+      <TextLoop
+        interval={interval}
+        transition={{ duration: transitionDuration }}
+      >
+        {initialTexts.map((text, index) => {
+          const isFirstElement = index === 0 && isFirstRender;
+          const duration = isFirstElement ? 0 : textAnimationDuration;
+          const repeat = isFirstElement ? 0 : textAnimationRepeat;
+
+          return (
+            <TextShimmerWave
+              repeat={repeat}
+              duration={duration}
+              spread={spread}
+              className={textColorStyle + gradientColorStyle + className}
+              {...rest}
+              key={text + index}
+            >
+              {text}
+            </TextShimmerWave>
+          );
+        })}
+      </TextLoop>
+    </div>
+  );
+};
