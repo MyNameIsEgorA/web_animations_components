7dc4ac2381c17b6784b70213bfe767d944f598a1
Egor Anakhin <eanahin8v@mail.ru>
2025-02-20 22:34:59 +0300

    deps: aliases initialized deps: morhing dialog component initialized

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import * as path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@lib": path.resolve(__dirname, "src/lib"),
+    },
+  },
+});
